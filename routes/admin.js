1bda4971a9b250dc1672e1a9f85a3e19fe8123c2
hamalaa8 <aleksi.hamalainen@aalto.fi>
2016-03-27 14:47:51 +0300

    Name added. Admin started

---
+++ routes/admin.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+/* GET users listing. */
+
+
+module.exports = router;
