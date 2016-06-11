cd9dbb291456a18b12063deb20f967f3c5b1ae3a
hamalaa8 <aleksi.hamalainen@aalto.fi>
2016-06-11 20:32:30 +0300

    admin-obj lisätty configgiin

---
+++ config/admin-obj.js
@@ -0,0 +1,7 @@
+module.export = {
+		firstname: "Luukas",
+		lastname: "Castren",
+		email: "admin@admin.com",
+		password: "admin",
+		admin: true
+}
--- config/app-config.js
+++ config/app-config.js
@@ -1,3 +1,5 @@
+var adminObj = require('./admin-obj')
+
 var env = process.env.ENVIRONMENT_NAME || "local";
 
 var prod = {
@@ -6,13 +8,7 @@ var prod = {
 
 var local = {
 	databaseUrl: "postgresql://junkkari:md565dc8059e1f8abdcf75606e8f61b6d29@localhost:5432/Junction",
-	admin: {
-		firstname: "Luukas",
-		lastname: "Castren",
-		email: "admin@admin.com",
-		password: "admin",
-		admin: true
-	}
+	admin: adminObj
 };
 
 if(env === "local") {
