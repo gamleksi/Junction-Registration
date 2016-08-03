edbfe320f5d8606217bd55cadd63eac6e625aa82
hamalaa8 <aleksi.hamalainen@aalto.fi>
2016-08-03 15:22:41 +0300

    sengrid uncommented

--- sendgrid/sendgrid.js
+++ sendgrid/sendgrid.js
@@ -1,104 +1,104 @@
-  var EventEmitter = require('events');
-  var event = new EventEmitter();
-  var helper = require('sendgrid').mail;
-  var travelValues= require("../TRAVEL_VALUES.js")
-  var from_email = new helper.Email(process.env.EMAIL_FROM)  
+  // var EventEmitter = require('events');
+  // var event = new EventEmitter();
+  // var helper = require('sendgrid').mail;
+  // var travelValues= require("../TRAVEL_VALUES.js")
+  // var from_email = new helper.Email(process.env.EMAIL_FROM)  
   
-  var approvalMail = {
-    content: new helper.Content("text/plain", "some text here"),  
-    subject: "Registration",
-  };
+  // var approvalMail = {
+  //   content: new helper.Content("text/plain", "some text here"),  
+  //   subject: "Registration",
+  // };
 
-  var sg = require('sendgrid').SendGrid(process.env.SENDGRID_API_KEY)
-
-
-  var createNewRequest = function(requestBody) {
-      var request = sg.emptyRequest()
-      request.method = 'POST'
-      request.path = '/v3/mail/send'
-      request.body = requestBody
-      return request;
-  };
-
-  module.exports = {
+  // var sg = require('sendgrid').SendGrid(process.env.SENDGRID_API_KEY)
 
 
-    sendApprovalMails: function(emailObjects, callback) {
-      console.log("SEND APPROVALMAILS")
-      event.on('newMailSent', function(responseObject) {
-        console.log("EMITTED")
-        callback(responseObject);
-      });
+  // var createNewRequest = function(requestBody) {
+  //     var request = sg.emptyRequest()
+  //     request.method = 'POST'
+  //     request.path = '/v3/mail/send'
+  //     request.body = requestBody
+  //     return request;
+  // };
 
-      var emails = Object.keys(emailObjects);
+  // module.exports = {
 
-      var to_email = new helper.Email(emails[0])
 
-      mail = new helper.Mail(from_email, "You have been accepted to Junction", to_email, approvalMail.content);  
-      mail.personalizations[0].addSubstitution({"-email-":emails[0]}) 
-      mail.personalizations[0].addSubstitution({"-travel-":travelValues.message(emailObjects[emails[0]].travelReimbursement)})
+  //   sendApprovalMails: function(emailObjects, callback) {
+  //     console.log("SEND APPROVALMAILS")
+  //     event.on('newMailSent', function(responseObject) {
+  //       console.log("EMITTED")
+  //       callback(responseObject);
+  //     });
+
+  //     var emails = Object.keys(emailObjects);
+
+  //     var to_email = new helper.Email(emails[0])
+
+  //     mail = new helper.Mail(from_email, "You have been accepted to Junction", to_email, approvalMail.content);  
+  //     mail.personalizations[0].addSubstitution({"-email-":emails[0]}) 
+  //     mail.personalizations[0].addSubstitution({"-travel-":travelValues.message(emailObjects[emails[0]].travelReimbursement)})
       
-      var reverseHash = emailObjects[emails[0]].hash.split("").reverse().join("");
+  //     var reverseHash = emailObjects[emails[0]].hash.split("").reverse().join("");
       
-      mail.personalizations[0].addSubstitution({"-name-":emailObjects[emails[0]].firstname}) 
-      mail.personalizations[0].addSubstitution({"-hash-":reverseHash}) 
+  //     mail.personalizations[0].addSubstitution({"-name-":emailObjects[emails[0]].firstname}) 
+  //     mail.personalizations[0].addSubstitution({"-hash-":reverseHash}) 
  
 
-      sg.emptyRequest();
+  //     sg.emptyRequest();
 
 
-      for(var i in emails) {
-        if(i > 0){
-          to_email = new helper.Email(emails[i]);  
-          var personalization = new helper.Personalization();
-          var reverseHash = emailObjects[emails[i]].hash.split("").reverse().join("");
+  //     for(var i in emails) {
+  //       if(i > 0){
+  //         to_email = new helper.Email(emails[i]);  
+  //         var personalization = new helper.Personalization();
+  //         var reverseHash = emailObjects[emails[i]].hash.split("").reverse().join("");
 
-          personalization.addTo(to_email);
-          personalization.addSubstitution({"-email-":emails[i]})
-          personalization.addSubstitution({"-travel-":travelValues.message(emailObjects[emails[i]].travelReimbursement)}) 
-          personalization.addSubstitution({"-first_name-":emailObjects[emails[i]].firstname}) 
-          personalization.addSubstitution({"-hash-":reverseHash}) 
-          mail.addPersonalization(personalization);
-        }
-      }
-      var requestBody = mail.toJSON()
-      requestBody.template_id = process.env.SENDGRID_ACCEPTED_TEMPLATE_ID
-      console.log("request body")
-      console.log(requestBody);
+  //         personalization.addTo(to_email);
+  //         personalization.addSubstitution({"-email-":emails[i]})
+  //         personalization.addSubstitution({"-travel-":travelValues.message(emailObjects[emails[i]].travelReimbursement)}) 
+  //         personalization.addSubstitution({"-first_name-":emailObjects[emails[i]].firstname}) 
+  //         personalization.addSubstitution({"-hash-":reverseHash}) 
+  //         mail.addPersonalization(personalization);
+  //       }
+  //     }
+  //     var requestBody = mail.toJSON()
+  //     requestBody.template_id = process.env.SENDGRID_ACCEPTED_TEMPLATE_ID
+  //     console.log("request body")
+  //     console.log(requestBody);
       
       
-      var request = createNewRequest(requestBody);
+  //     var request = createNewRequest(requestBody);
       
-      sg.API(request, function (response) {
-        console.log("Mail")
-        console.log(response.statusCode)
-        // console.log("BODY FROM SendGrid")
-        // console.log(response.body)
-        // console.log(response.headers)
-        event.emit('newMailSent', response.statusCode);
-      });
+  //     sg.API(request, function (response) {
+  //       console.log("Mail")
+  //       console.log(response.statusCode)
+  //       // console.log("BODY FROM SendGrid")
+  //       // console.log(response.body)
+  //       // console.log(response.headers)
+  //       event.emit('newMailSent', response.statusCode);
+  //     });
       
-      },
+  //     },
       
-      sendRegisterConfirmation: function(email, firstname){
-        var to_email = new helper.Email(email);
-        var mail = new helper.Mail(from_email, "Junction registeration", to_email, approvalMail.content);  
-        mail.personalizations[0].addSubstitution({"%email%":email});
-        mail.personalizations[0].addSubstitution({"%first_name%": firstname});  
-        sg.emptyRequest();
-        console.log("mail.personalizations[0]")
-        console.log(mail.personalizations[0])
+  //     sendRegisterConfirmation: function(email, firstname){
+  //       var to_email = new helper.Email(email);
+  //       var mail = new helper.Mail(from_email, "Junction registeration", to_email, approvalMail.content);  
+  //       mail.personalizations[0].addSubstitution({"%email%":email});
+  //       mail.personalizations[0].addSubstitution({"%first_name%": firstname});  
+  //       sg.emptyRequest();
+  //       console.log("mail.personalizations[0]")
+  //       console.log(mail.personalizations[0])
 
-        var requestBody = mail.toJSON();
-        requestBody.template_id = process.env.SENDGRID_REGISTER_TEMPLATE_ID;  
-        var request = createNewRequest(requestBody);
+  //       var requestBody = mail.toJSON();
+  //       requestBody.template_id = process.env.SENDGRID_REGISTER_TEMPLATE_ID;  
+  //       var request = createNewRequest(requestBody);
         
-        sg.API(request, function (response) {
-          console.log("Mail");
-          console.log(response.statusCode);
-          console.log("BODY FROM SendGrid");
-          console.log(response.body)
-        });
-      }
+  //       sg.API(request, function (response) {
+  //         console.log("Mail");
+  //         console.log(response.statusCode);
+  //         console.log("BODY FROM SendGrid");
+  //         console.log(response.body)
+  //       });
+  //     }
 
-  };
+  // };
