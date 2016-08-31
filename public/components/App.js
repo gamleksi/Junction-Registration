6da7f35fcbaa62d8fa74caebbbd3cf310229219c
hamalaa8 <aleksi.hamalainen@aalto.fi>
2016-08-31 22:09:35 +0300

    fixed

--- public/components/App.js
+++ public/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-import AdminPanel from "./ui-Hackers/AdminPanel"
+import AdminPanel from "./ui-hackers/AdminPanel"
 
 const app = document.getElementById('container');
 ReactDom.render(<AdminPanel />, app)
